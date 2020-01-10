cd1ed625bb36f8a172824aedd1c3e6ede47f9a50
Alireza Sadraii <sadraiiali@gmail.com>
2020-01-10 22:47:47 +0330

    add make Binary scripts

---
+++ makeBin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm -r Bin
+mkdir Bin
+cp -r Agent/Lib/lib Bin
+cp Agent/src/sample_player Bin
+cp Agent/src/sample_coach Bin
+cp Agent/src/player.conf Bin
+cp Agent/src/coach.conf Bin
+cp -r Agent/src/formations-dt Bin
+cp Files/* Bin
